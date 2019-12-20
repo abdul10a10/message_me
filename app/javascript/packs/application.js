579a762eb6d02464056336f32537e77779539510
10a10khan <abdulrahman.khan@codoxysolutions.com>
2019-12-20 17:04:36 +0530

    bundle update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,12 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
+
+$(document).on('turbolinks:load',function(){
+    $('.ui.dropdown').dropdown();
+    })
